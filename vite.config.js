81ddcf976938497432fafccc84fd94a42037aed9
Viktoriia <lyudkevych.v@gmail.com>
2025-06-16 09:22:17 -0700

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/weather-report/',
   plugins: [react()],
   test: {
     // jest config here
